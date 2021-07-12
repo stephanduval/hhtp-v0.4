339a669080ebd10d2f017be56d5faecda39935bb
Stephan DuVal <stephan.duval@gmail.com>
2021-07-11 23:10:24 -0600

    End of day commit, added PhotoSpace Redux scaffolding

--- src/containers/PhotoSpace/actions.js
+++ src/containers/PhotoSpace/actions.js
@@ -4,3 +4,8 @@ export const setCorrectResponseArray = (correctResponseArray) => ({
     type: ActionTypes.CORRECT_RESPONSE_ARRAY,  // from the constants file
     payload: correctResponseArray
 })
+
+export const setUserResponseArray = (userResponseArray) => ({
+    type: ActionTypes.USER_RESPONSE_ARRAY,  // from the constants file
+    payload: userResponseArray
+})
---
+++ src/containers/PhotoSpace/selectors.js
@@ -0,0 +1,21 @@
+import { createSelector } from "reselect";
+
+const photoSpaceState = (state) => state.photoSpace;  // state.name matches the name used in combine reducers in store.js
+
+// this is the old way: 
+// export const makeSelectUsers = homePage(state).user
+// but we will use the reselector library module
+// this will bind homePageUSers and return the users variable in the homePage part of the store
+export const makeselectUserResponseArray = createSelector(  //'make* is a naming convention for selectors
+photoSpaceState,  // get the global store state for state.homepage
+    photoSpace => photoSpace.makeselectUserResponseArray //find the data within the store.js reducer name (homePage) and users within that: homePage.users
+     );
+
+export const makeselectUserResponseArray = createSelector(  //'make* is a naming convention for selectors
+photoSpaceState,  // get the global store state for state.homepage
+    photoSpace => photoSpace.makeselectUserResponseArray //find the data within the store.js reducer name (homePage) and users within that: homePage.users
+     );
+
+
+//     userResponseArray
+//     correctResponseArray
