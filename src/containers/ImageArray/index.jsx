c9e3a38323d1b3ac81a05867b94131565802d8ec
Stephan DuVal <stephan.duval@gmail.com>
2021-07-19 00:36:53 -0600

    end of day commit

--- src/containers/ImageArray/index.jsx
+++ src/containers/ImageArray/index.jsx
@@ -84,8 +84,27 @@ const ImageArray = () => {  // this destructing allows us to use onInputChange i
     } 
 
   const arraywithPredictives = predictiveSet(slicedRandomizedImageArray) 
+/*
+  function ImageArrayCorrectAnswersPreNBack(arraywithPredictives,PredictiveFileNameArray) {
+    let arr = [];
+    arraywithPredictives.forEach(item)
+    if((PredictiveFileNameArray.includes(item))
+    {
+      arr.push('p');
+    }
+    else {
+      arr.push('o');
+    })
+    return arr;
+      ;
 
 
+
+  }
+
+  
+  const testingArray = ImageArrayCorrectAnswersPreNBack(arraywithPredictives,PredictiveFileNameArray);
+*/
 /*
 function spliceNBacksIntoArray(array) {
     /*
@@ -195,6 +214,9 @@ setPredictiveImageFileNameLength: {ReduxPredictiveFileNameArrayLength}
 </p>
 <p>
   arraywithPredictives: {arraywithPredictives}
+</p>
+<p>
+  {testingArray}
 </p>
 </div>
     )
