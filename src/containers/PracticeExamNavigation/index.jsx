6d981c53fb4512eb99e85adaa5e938c98d156819
Stephan DuVal <stephan.duval@gmail.com>
2021-10-23 15:16:41 -0600

    Separated the button logic statements

--- src/containers/PracticeExamNavigation/index.jsx
+++ src/containers/PracticeExamNavigation/index.jsx
@@ -115,33 +115,59 @@ const [show, setShow] = useState(false)
 
 let practiceArraySourceSlice = practiceImageArrayFromRedux[NBackState].slice(32,-9);
 
+
+const checkLogic = (aParam,Bparam) => {
+if (aParam == 1){
+    console.log("aParam",aParam)
+    return
+
+}else{
+    console.log("Testing the Logic",aParam,Bparam)
+    return
+}
+}
+
+
+const practiceTwoBackCheckResult = (imageSourceStringParam) => {
+    console.log("TwoBackCheckResultRan")
+    if (imageSourceStringParam.includes("Two-Back-Hits",0) && practiceImageArrayFromRedux[NBackState] == practiceImageArrayFromRedux[NBackState-2] ) {     
+        return {modalTitle: "CORRECT!", modalMessage: "The image you are looking at appeared two images prior to this one. You're getting it"};
+    }
+    if (imageSourceStringParam.includes("Two-Back-Hits",0) && practiceImageArrayFromRedux[NBackState] !== practiceImageArrayFromRedux[NBackState-2] ) {     
+        return {modalTitle: "Wrong Answer!", modalMessage: "You have not seen this image before.  However you may see it repeated later."};
+} else {
+    return {modalTitle: Error, modalMessage: Error }
+}
+
+}
+
 const practiceCheckResult = (practiceExamResponseParam,imageSourceStringParam) => {
-        if (practiceExamResponseParam === 1 && imageSourceStringParam.includes("Two-Back-Hits",0) && practiceImageArrayFromRedux[NBackState] == practiceImageArrayFromRedux[NBackState-2] ) {     
+        if (practiceExamResponseParam > 0 && imageSourceStringParam.includes("Two-Back-Hits",0) && practiceImageArrayFromRedux[NBackState] == practiceImageArrayFromRedux[NBackState-2] ) {     
             return {modalTitle: "CORRECT!", modalMessage: "The image you are looking at appeared two images prior to this one. You're getting it"};
         }
-        if (practiceExamResponseParam === 1 && imageSourceStringParam.includes("Two-Back-Hits",0) && practiceImageArrayFromRedux[NBackState] !== practiceImageArrayFromRedux[NBackState-2] ) {     
+        if (practiceExamResponseParam > 0 && imageSourceStringParam.includes("Two-Back-Hits",0) && practiceImageArrayFromRedux[NBackState] !== practiceImageArrayFromRedux[NBackState-2] ) {     
             return {modalTitle: "Wrong Answer!", modalMessage: "You have not seen this image before.  However you may see it repeated later."};
         }
-        if (practiceExamResponseParam === 1 && imageSourceStringParam.includes("RemainingPictures",0)) {     
+        if (practiceExamResponseParam > 1 && imageSourceStringParam.includes("RemainingPictures",0)) {     
             return {modalTitle: "Wrong Answer!", modalMessage: "The image you are looking at is a unique Image. You have not seen it before"};
         }
-        if (practiceExamResponseParam === 2 && imageSourceStringParam.includes("RemainingPictures",0)) {     
+        if (practiceExamResponseParam == 0 && imageSourceStringParam.includes("RemainingPictures",0)) {     
             return {modalTitle: "Wrong Answer!", modalMessage: "Click this button if the image is the same as one if the images you were asked to remember.  You were not asked to remember any images in this practice exam."};
         }
-        if (practiceExamResponseParam === 3 && imageSourceStringParam.includes("RemainingPictures",0)) {     
+        if (practiceExamResponseParam < 0 && imageSourceStringParam.includes("RemainingPictures",0)) {     
             return {modalTitle: "CORRECT!", modalMessage: "This is a unique image.  You have not seen it before."};
         }
-        if (practiceExamResponseParam === 2 && imageSourceStringParam.includes("Two-Back-Hits",0) && practiceImageArrayFromRedux[NBackState] == practiceImageArrayFromRedux[NBackState-2] ) {     
+        if (practiceExamResponseParam == 0 && imageSourceStringParam.includes("Two-Back-Hits",0) && practiceImageArrayFromRedux[NBackState] == practiceImageArrayFromRedux[NBackState-2] ) {     
             return {modalTitle: "Wrong Answer!", modalMessage: "This image appeared two images ago.  When you see an image repreat with one image in between click the \"Same As 2 Photos Back\" button"};
         }
-        if (practiceExamResponseParam === 3 && imageSourceStringParam.includes("Two-Back-Hits",0) && practiceImageArrayFromRedux[NBackState] == practiceImageArrayFromRedux[NBackState-2] ) {     
+        if (practiceExamResponseParam < 0 && imageSourceStringParam.includes("Two-Back-Hits",0) && practiceImageArrayFromRedux[NBackState] == practiceImageArrayFromRedux[NBackState-2] ) {     
             return {modalTitle: "Wrong Answer!", modalMessage: "This image appeared two images ago.  When you see an image repreat with one image in between click the \"Same As 2 Photos Back\" button"};
     } else {
-        return {modalTitle: "NoTitle", modalMessage: "No Message"}
+        return {modalTitle: practiceExamResponseParam, modalMessage: imageSourceStringParam }
     }
 
 };
-let modalCheckResult = practiceCheckResult('a',practiceArraySourceSlice);
+let modalCheckResult = practiceTwoBackCheckResult(practiceArraySourceSlice);
 
 
 // we are object destructuring the result
@@ -149,7 +175,7 @@ let modalCheckResult = practiceCheckResult('a',practiceArraySourceSlice);
 
 
 
-// Has to export for the Modal Message
+// Has to export for the Modal Message"
 // Modal Title
 //
 
@@ -163,13 +189,13 @@ let modalCheckResult = practiceCheckResult('a',practiceArraySourceSlice);
             <div className="container">    
                 <div className="buttonSpace">
         
-            <Button color ="primary" variant="contained" stringvalue={"Same as *n* photos Back"} onClick={()=>{practiceCheckResult(11,practiceArraySourceSlice);newPracticeNBackState(NBackState);checkIfTestIsComplete();}}>"W" - Same as 2 photos Back
+            <Button color ="primary" variant="contained" stringvalue={"Same as *n* photos Back"} onClick={()=>{let modalCheckResult=practiceTwoBackCheckResult(practiceArraySourceSlice);newPracticeNBackState(NBackState);checkIfTestIsComplete();}}>"W" - Same as 2 photos Back
                 </Button>
         
-                <Button color ="default" variant="contained" stringvalue={"O - Predictive"} onClick={()=>{practiceCheckResult(22,practiceArraySourceSlice);newPracticeNBackState(NBackState);checkIfTestIsComplete()}}>"O" - Predictive - I was told to remember this
+                <Button color ="default" variant="contained" stringvalue={"O - Predictive"} onClick={()=>{practiceCheckResult(0,practiceArraySourceSlice);newPracticeNBackState(NBackState);checkIfTestIsComplete()}}>"O" - Predictive - I was told to remember this
                 </Button>
         
-                <Button color ="secondary" variant="contained" stringvalue={"S - Unique Image"} onClick={()=>{practiceCheckResult(33,practiceArraySourceSlice);newPracticeNBackState(NBackState);checkIfTestIsComplete()}}>"S" - Unique Image
+                <Button color ="secondary" variant="contained" stringvalue={"S - Unique Image"} onClick={()=>{practiceCheckResult(-1,practiceArraySourceSlice);newPracticeNBackState(NBackState);checkIfTestIsComplete()}}>"S" - Unique Image
                 </Button>
         
                 </div>
