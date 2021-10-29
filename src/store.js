f958a52b1b3d08f6b3f2b459085ed38b9811838e
Stephan DuVal <stephan.duval@gmail.com>
2021-10-28 23:31:57 -0600

    Heroku again changed the file name of cognitive-Reappraisal-Array to lowercase first letter

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ import imageArray from './containers/n-back-image-array/reducers'
 import craNavigation from "./containers/CognitiveReappraisalNavigation/reducers";
 import practiceImageArrayRedux from "./containers/Practice-n-back-image-array/reducers";
 import practiceExamNavigation from "./containers/PracticeExamNavigation/reducers";
-import crImageArrayToStore from "./containers/Cognitive-Reappraisal-Array/reducers";
+import crImageArrayToStore from "./containers/cognitive-Reappraisal-Array/reducers";
 
 const reducers = combineReducers({examNavigationReducer: examNavigation, photoSpaceReducer: photoSpace, nBackSettingsReducer: nBackSettings, imageArrayReducer: imageArray, craNavigationReducer: craNavigation, practiceImageArrayReducer: practiceImageArrayRedux, practiceExamNavigationReducer: practiceExamNavigation, cRImageArrayReducer: crImageArrayToStore});
 // console.log("reducers ran", createStore(reducers)) //
